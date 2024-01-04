e275c775e4227e3df6d55a638672c8e55ff9dfaf
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-01-03 21:13:45 -0300

    Primeira lógica do rotate

--- src/brick.cpp
+++ src/brick.cpp
@@ -21,9 +21,11 @@ Brick::Brick() {
     matrix[4] = 1;
     matrix[5] = 1;
 
-    position = sf::Vector2f(262.5, 262.5);
+    position = sf::Vector2f(10, 10);
 
     size = 3;
+
+    rotate_clockwise();
 
 }
 
@@ -38,7 +40,13 @@ void Brick::go_left() {
 }
 
 void Brick::rotate_clockwise() {
-    throw std::logic_error("TODO");
+    std::vector<int> aux(matrix.size(), 0);
+
+    for (long unsigned int i = 0; i < matrix.size(); i++) {
+        aux[(size - i / size - 1) + (i % size) * size] = matrix[i];
+    }
+
+    matrix = aux;
 }
 
 void Brick::accelerate() {
@@ -55,9 +63,14 @@ void Brick::update() {
 
 void Brick::draw_at(sf::RenderWindow *window) {
     for (long unsigned int i = 0; i < matrix.size(); i++) {
+        shape.setPosition(sf::Vector2f(position.x * brick_size, position.y * brick_size) + sf::Vector2f((i % size) * brick_size, (i / size) * brick_size));
+        
         if (matrix[i]) {
-            shape.setPosition(position + sf::Vector2f((i % size) * brick_size, static_cast<int>(i / size) * brick_size));
-            window->draw(shape);
-        }    
+            shape.setFillColor(sf::Color::Green);
+        } else {
+            shape.setFillColor(sf::Color::Blue);
+        }
+
+        window->draw(shape);
     }
 }
